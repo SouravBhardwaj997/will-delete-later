b7f5731bcb8f7ed4c33ab1211c07aa4a3aefdc4e
Sourav Bhardwaj <souravbhardwaj997@gmail.com>
2026-04-01 16:54:31 +0530

    Some Commit Message

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-import fs from "fs";
-import { exec } from "child_process";
+import fs from "fs/promises";
+import { promisify } from "node:util";
+import { exec as originalExec } from "node:child_process";
 
+const exec = promisify(originalExec);
 const string = "Hello ";
 
 Array.from({ length: 10 }).map(async (el, i) => {
-  fs.appendFile("./temp.txt", string, "utf8", () => {
-    const command =
-      'git add . && git commit -m "Some Commit Message" && git push origin master';
+  await fs.appendFile("./temp.txt", string, "utf8");
+  const command =
+    'git add . && git commit -m "Some Commit Message" && git push origin master';
 
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`exec error: ${error}`);
-        return;
-      }
-      console.log(`stdout: ${stdout}`);
-      console.error(`stderr: ${stderr}`);
-    });
-    console.log("Commit DONE", i);
+  await exec(command, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`exec error: ${error}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.error(`stderr: ${stderr}`);
   });
+  console.log("Commit DONE", i);
 });
