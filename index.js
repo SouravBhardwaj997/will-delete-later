dd9f10f0770ca361dbf611e93cf1e91a7a7168b3
Sourav Bhardwaj <souravbhardwaj997@gmail.com>
2026-04-01 16:56:32 +0530

    Some Commit Message

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import fs from "fs/promises";
 import { promisify } from "node:util";
-import { exec as originalExec } from "node:child_process";
+import { execSync, exec as originalExec } from "node:child_process";
 
 const exec = promisify(originalExec);
 const string = "Hello ";
@@ -10,13 +10,6 @@ Array.from({ length: 10 }).map(async (el, i) => {
   const command =
     'git add . && git commit -m "Some Commit Message" && git push origin master';
 
-  await exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
-  });
+  await execSync(command);
   console.log("Commit DONE", i);
 });
