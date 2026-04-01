5ab3cadd01d4e8575179d7781785bfd979fd497b
Sourav Bhardwaj <souravbhardwaj997@gmail.com>
2026-04-01 16:50:09 +0530

    Some Commit Message

--- index.js
+++ index.js
@@ -3,19 +3,19 @@ import { exec } from "child_process";
 
 const string = "Hello ";
 
-Array.from({ length: 10 }).map((el) => {
-  fs.appendFile("./temp.txt", string, "utf8", () => {});
+Array.from({ length: 10 }).map(async (el) => {
+  fs.appendFile("./temp.txt", string, "utf8", () => {
+    const command =
+      'git add . && git commit -m "Some Commit Message" && git push origin master';
 
-  const command =
-    'git add . && git commit -m "Some Commit Message" && git push origin master';
-
-  exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error(`exec error: ${error}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
+    exec(command, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`stdout: ${stdout}`);
+      console.error(`stderr: ${stderr}`);
+    });
+    console.log("Commit DONE", el);
   });
-  console.log("Commit DONE", el);
 });
